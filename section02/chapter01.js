0d22cbbd67ab7581ba10adc8bfa0196cf701aefe
JisooPyo <audwl326@gmail.com>
2026-02-26 21:30:46 +0900

    s02-c01: Truthy와 Falsy

---
+++ section02/chapter01.js
@@ -0,0 +1,37 @@
+// 1. Falsy한 값
+let f1 = undefined;
+let f2 = null;
+let f3 = 0;
+let f4 = -0;
+let f5 = NaN;
+let f6 = "";
+let f7 = 0n;
+
+if (!f7) {
+  console.log("falsy");
+}
+
+// 2. Truthy 한 값
+// -> 7가지 Falsy 한 값들 제외한 나머지 모든 값
+let t1 = "hello";
+let t2 = 123;
+let t3 = [];
+let t4 = {};
+let t5 = () => {};
+
+if (t5) {
+  console.log("Truthy");
+}
+
+// 3. 활용 사례
+
+function printName(person) {
+  if (!person) {
+    console.log("person의 값이 없음");
+    return;
+  }
+  console.log(person.name);
+}
+
+let person;
+printName(person);
