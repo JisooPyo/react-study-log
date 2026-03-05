72e23a38c2a0e1c46c28c28d8248ff9bce128781
JisooPyo <audwl326@gmail.com>
2026-03-05 22:28:40 +0900

    s03-c03: Node.js 사용하기

---
+++ section03/src/index.js
@@ -0,0 +1,5 @@
+console.log("안녕 Node.js");
+
+// package.json에서 scripts에 "start"를 key로 하여 "node src/index.js"를 입력해주고
+// 터미널에서 "npm run start"를 실행하면 node src/index.js를 실행해준다.
+// script는 npm run 뒤에 key를 붙여서 실행할 수 있으며 매크로를 설정해줄 수 있다.
